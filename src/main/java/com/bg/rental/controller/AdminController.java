dca0611893c2fe66b17e0c670567319ca2b96877
hannguyend <test.himlam@gmail.com>
2015-09-22 11:22:03 +0700

    Clean up a bit

--- src/main/java/com/bg/rental/controller/AdminController.java
+++ src/main/java/com/bg/rental/controller/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.bg.rental.entity.User;
 import com.bg.rental.service.UserService;
 
 @Controller
